640c3cd8f549bf2bdd317126be65c4fc862bf239
Thomas Gauvin <thomasgauvin.cu@gmail.com>
2022-12-10 20:26:54 -0500

    added comment

--- shared/app-settings.ts
+++ shared/app-settings.ts
@@ -1,9 +1,9 @@
 // source: https://github.com/utterance/utterances-oauth/blob/master/src/app-settings.ts
 
 export const settings = {
-    client_id: process.env.CLIENT_ID!,
-    client_secret: process.env.CLIENT_SECRET!,
-    state_password: process.env.STATE_PASSWORD!,
+    client_id: process.env.CLIENT_ID, //ensure these application settings are set in the host azure function
+    client_secret: process.env.CLIENT_SECRET,
+    state_password: process.env.STATE_PASSWORD,
     // bot_token: process.env.BOT_TOKEN!,
     // origins: process.env.ORIGINS!.split(",").map(x => x.trim()),
     // webhook_secret: process.env.WEBHOOK_SECRET!
